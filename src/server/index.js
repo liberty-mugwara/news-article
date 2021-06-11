ef5251b0aa7b7706d0290f23d644be22681b61b7
Liberty Mugwara <libertymugwara@live.com>
2021-06-11 06:03:13 +0200

    feat: Add routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,31 @@
 import { config } from "dotenv";
 config();
-import express, { static, json, urlencoded } from "express";
+import express, {
+  json as expressJson,
+  urlencoded as expressUrlencoded,
+} from "express";
 import helmet from "helmet";
 import cors from "cors";
+import chalk from "chalk";
+import appRouter from "./router.js"
 
 const app = express();
-
 app.use(
-  static("build"),
+  express.static("dist"),
   helmet(),
   cors(),
-  json(),
-  urlencoded({ extended: false })
+  expressJson(),
+  expressUrlencoded({ extended: false }),
 );
 
-console.log("api key is: " + process.env.API_KEY);
+app.use("analyze",appRouter)
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(chalk.green(`App listening on port ${port}`));
+  console.log(
+    `${chalk.bold("App is being served at:")} ${chalk.yellow(
+      `http://localhost:${port}/`
+    )}`
+  );
+});
