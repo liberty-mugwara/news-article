d10cb5854664792ad04edc9645ff30940a9996c9
Liberty Mugwara <libertymugwara@live.com>
2021-06-16 06:18:17 +0200

    feat: Add support to remove app url

--- src/client/js/config.js
+++ src/client/js/config.js
@@ -1,3 +1,6 @@
+const localUrl = "http://localhost:8080";
+const remoteUrl = "https://mugs-sentiment.herokuapp.com"
+
 export const config = {
-  apiBaseUrl: "http://localhost:8080",
+  apiBaseUrl: remoteUrl,
 };
